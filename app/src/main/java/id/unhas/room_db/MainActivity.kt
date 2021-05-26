bc4f7c7eb369fd4c52c0a8865d739f34c1a84276
“serwilhan” <serwilhan@gmail.com>
2021-05-26 21:02:31 +0800

    first commit

---
+++ app/src/main/java/id/unhas/room_db/MainActivity.kt
@@ -0,0 +1,11 @@
+package id.unhas.room_db
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
